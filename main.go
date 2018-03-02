caa656f69ddb4fa40db3093ae316b3d0d15f0c9e
Zhao JiaJin <zhaojj@rd.netease.com>
2018-03-02 14:45:53 +0800

    reload config

--- main.go
+++ main.go
@@ -7,13 +7,23 @@ import(
 	"time"
 	"errors"
 	"flag"
+	"sync"
+	"io/ioutil"
 )
+
+type LogCfg struct{
+	sync.RWMutex
+	logmap map[string][]string//{disk:path}
+}
+
 
 var(
 	NoFileSysErr = errors.New("can not get file sys info")
 	TypeCvtErr = errors.New("type convert error")
-)
 
+	cfgfile string
+	cfgmtime time.Time
+)
 
 
 func getAccTime(info os.FileInfo)(time.Time,error){
@@ -33,9 +43,48 @@ func getModTime(info os.FileInfo)(time.Time){
 	return info.ModTime()
 }
 
+
+func buildcfgmap(cfgstr string){
+	log.Println(cfgstr)
+}
+
+func loadcfg()error{
+	cfginfo,err := os.Stat(cfgfile)
+	if err != nil{
+		return err
+	}
+	newmtime := getModTime(cfginfo)
+	if newmtime == cfgmtime{
+		return nil
+	}
+	cfgmtime = newmtime
+	log.Println("update config from file:",cfgfile)
+	dat, err := ioutil.ReadFile(cfgfile)
+	if err != nil{
+		return err
+	}
+	buildcfgmap(string(dat))
+	return nil
+}
+
+func reloadcfg(){
+	for{
+		time.Sleep(time.Second * 1)
+		err := loadcfg()
+		if err != nil{
+			log.Panic(err)
+		}
+	}
+}
+
 func main(){
-	var cfgfile string
 	flag.StringVar(&cfgfile,"c","config","config file")
 	flag.Parse()
 	log.Println("using config file:",cfgfile)
+	err := loadcfg()
+	if err != nil{
+		log.Panic(err)
+	}
+	go reloadcfg()
+	select {}
 }
