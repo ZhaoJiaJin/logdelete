5ca2d7554b7d0da78fc8def487eecf56743b07e7
Zhao JiaJin <zhaojj@rd.netease.com>
2018-03-02 15:22:36 +0800

    add mount point

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import(
 	"flag"
 	"sync"
 	"io/ioutil"
+	"strings"
 )
 
 type LogCfg struct{
@@ -23,6 +24,7 @@ var(
 
 	cfgfile string
 	cfgmtime time.Time
+	cfg LogCfg
 )
 
 
@@ -43,9 +45,37 @@ func getModTime(info os.FileInfo)(time.Time){
 	return info.ModTime()
 }
 
+func leadwith(s1,s2 string)bool{
+	if len(s1) < len(s2){
+		return false
+	}
+	return s1[:len(s2)] == s2
+}
 
-func buildcfgmap(cfgstr string){
-	log.Println(cfgstr)
+func buildcfgmap(cfgstr string,mountpts []string){
+	cfg.Lock()
+	defer cfg.Unlock()
+	cfg.logmap = make(map[string][]string)
+	for _,logpath := range strings.Split(cfgstr,"\n"){
+		if len(logpath) == 0{
+			continue
+		}
+		if logpath[0] != '/'{
+			log.Println("[ERROR]路径必须是绝对路径:",logpath)
+		}
+		//build map
+		for _,mp := range mountpts{
+			if leadwith(logpath,mp){
+				_,present := cfg.logmap[mp]
+				if !present{
+					cfg.logmap[mp] = make([]string,0)
+				}
+				cfg.logmap[mp] = append(cfg.logmap[mp],logpath)
+				log.Println("[add]add",logpath,"to mount point",mp)
+				break
+			}
+		}
+	}
 }
 
 func loadcfg()error{
@@ -63,7 +93,11 @@ func loadcfg()error{
 	if err != nil{
 		return err
 	}
-	buildcfgmap(string(dat))
+	mountpints, err := getmountpoint()
+	if err != nil{
+		return err
+	}
+	buildcfgmap(string(dat),mountpints)
 	return nil
 }
 
@@ -77,14 +111,37 @@ func reloadcfg(){
 	}
 }
 
+func getmountpoint()([]string,error){
+	dat, err := ioutil.ReadFile("/proc/mounts")
+	if err != nil{
+		return []string{},err
+	}
+	recs := strings.Split(string(dat),"\n")
+	ans := make([]string,0)
+	for i:=0; i < len(recs); i ++{
+		sps := strings.Split(recs[i]," ")
+		if len(sps) > 2 && strings.Contains(sps[0],"/dev"){
+			ans = append(ans,sps[1])
+		}
+	}
+	for i:=0; i < len(ans); i ++{
+		for j:=i+1; j < len(ans); j ++{
+			if len(ans[i])< len(ans[j]){
+				ans[i], ans[j] = ans[j], ans[i]
+			}
+		}
+	}
+	return ans,nil
+}
+
 func main(){
 	flag.StringVar(&cfgfile,"c","config","config file")
 	flag.Parse()
 	log.Println("using config file:",cfgfile)
+	cfg.logmap = make(map[string][]string)
 	err := loadcfg()
 	if err != nil{
 		log.Panic(err)
 	}
-	go reloadcfg()
-	select {}
+	reloadcfg()
 }
